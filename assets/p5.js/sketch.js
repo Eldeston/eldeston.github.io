728cb9d49079cd2fc5f081ea0498da5b00380c79
Eldeston <fremfredric@gmail.com>
2025-05-04 15:53:09 +0400

    Optimized p5.js and improved FPS debugging

--- assets/p5.js/sketch.js
+++ assets/p5.js/sketch.js
@@ -1,84 +1,95 @@
 const noiseScale = 2.0;
-const noiseSpeed = 0.25 * 0.001;
+const noiseSpeed = 0.5 * 0.001;
 
-function squared(x){
-  return x * x;
-}
-
-function mix(x, y, z){
-  return x + (y - x) *z;
-}
-
-function coordFract(x, axis){
-  // return x;
-  return fract(x / axis) * axis;
-}
-
-function perlinCustom(x){
-  return noise(x) * 2.0 - 1.0;
-}
-
+// FPS counter
 function fpsCounter(x, y, z){
   let fps = frameCount / z;
   let fpsG = 1.0 - exp(-fps * 0.125);
   let fpsB = 1.0 - exp(-fps * 0.015625);
 
   fill(255, fpsG * 255, fpsB * 255);
+  textAlign(CENTER);
   text(fps, x, y);
 }
 
-function flowLine(currTime, y, z, w){
-  // Peak crest height
-  const peakCrest = 1.0;
+// Flow line using vertices
+function flowLine(currTime, crestHeight, z, w){
   // Get half of width
   const halfWidth = width * 0.5;
   // Get half of height multiplied
-  const halfHeight = (height / 6) * peakCrest * y;
+  const halfHeight = (height / 3) * crestHeight;
 
   // Decreasing vertex size will improve FPS
-  const vertexSize = width / 128;
+  const vertexCount = width / 128;
   // Store vertex step as a constant to save performance
   const vertexStep = 1 / width;
 
+  // Time at 1/4 speed
+  let timeFourth = currTime * 0.25;
+  // Time at 1/8 speed
+  let timeEighth = currTime * 0.125;
+
+  // Begin vertex shape
   beginShape();
   
   // Loop for calculating each vertex point
-  for(let i = -halfWidth; i < halfWidth; i += vertexSize){
+  for(let i = -halfWidth; i <= halfWidth; i += vertexCount){
+    // Get current step, can represent as the x axis of your canvas
     const currStep = (i * vertexStep + z) * noiseScale;
 
-    let noise0 = perlinCustom(currStep + currTime * 0.125);
-    let noise1 = perlinCustom(currStep - currTime * 0.25);
-    let noise2 = perlinCustom(currStep * 2.0 + currTime);
+    // Get noise depending on the x axis
+    let noise0 = noise(currStep + timeEighth);
+    let noise1 = noise(currStep - timeFourth);
+    let noise2 = noise(currStep * 2.0 + currTime);
   
-    vertex(i, (noise0 + noise1 + noise2) * halfHeight + w);
+    // Draw vertex node
+    vertex(i, (noise0 + noise1 + noise2 - 1.5) * halfHeight + w);
   }
   
+  // End vertex shape
   endShape();
 }
 
+// p5.js setup function
 function setup(){
+  // Set canvas to use entire window
   createCanvas(windowWidth, windowHeight);
 }
 
+// p5.js draw function
 function draw(){
+  // Calculate time per second
   let secondTime = millis() * noiseSpeed;
 
+  // Set background
   background(0, 0, 0, 255);
+  // Translate coordinates to center
   translate(width * 0.5, height * 0.5);
 
+  // Set fill to none
   noFill();
-  strokeWeight(2);
+  // Thicken stroke by 4
+  strokeWeight(4);
 
+  // Loop and calculate 8 lines for 3 bands of lines
   for(let i = 0; i <= 8; i++){
+    // Store wave size increment as a constant
+    const sizeIncrement = 0.5 + i / 8;
+
+    // Set red stroke color with transparency
     stroke(255, 128, 0, 64);
-    flowLine(secondTime, 0.5 + i / 8, 0, 0);
+    flowLine(secondTime, sizeIncrement, 0, 0);
 
+    // Set blue stroke color with transparency
     stroke(0, 128, 255, 64);
-    flowLine(secondTime, 0.5 + i / 8, 111, 0);
+    flowLine(secondTime, sizeIncrement, 111, 0);
 
+    // Set green stroke color with transparency
     stroke(0, 255, 0, 64);
-    flowLine(secondTime, 0.5 + i / 8, 977, 0);
+    flowLine(secondTime, sizeIncrement, 977, 0);
   }
 
-  fpsCounter(-width * 0.45, -height * 0.25, secondTime);
+  // Reset stroke color
+  stroke(0, 0, 0, 0);
+  fpsCounter(0, height * 0.125, secondTime);
 }
