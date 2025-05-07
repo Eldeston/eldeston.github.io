457b62a36970038b4e906cebd27c448d28238022
Eldeston <fremfredric@gmail.com>
2025-05-07 17:56:21 +0400

    Maximized optimization with p5.js

--- assets/p5.js/sketch.js
+++ assets/p5.js/sketch.js
@@ -2,8 +2,8 @@
 p5.disableFriendlyErrors = true;
 
 const speed = 4.0;
-const particles = 1024;
-const particleSize = 1;
+const particles = 8196;
+const particleSize = 2;
 const noiseScale = 1 / 128;
 const noiseRotations = 3 * 3.1416;
 
@@ -19,13 +19,15 @@ class particle{
     // Multiply velocity by speed and add to position to update and move the particle
     this.position.add(this.velocity.mult(speed));
 
-    // Calculate new velocity
-    let angle = perlinCustom(this.position.x, this.position.y) * noiseRotations;
+    // Calculate time to animate noise
+    let currTime = millis() * 0.0001;
+    // Calculate new velocity vector based on perlin noise
+    let angle = noise(this.position.x * noiseScale, this.position.y * noiseScale, currTime) * noiseRotations;
     // Create a 2D vector and assign new velocity
     this.velocity = createVector(Math.sin(angle), Math.cos(angle));
 
     // Finally, color the particle according to current velocity and time
-    stroke(Math.abs(this.velocity.x) * 255, Math.abs(this.velocity.y) * 255, Math.sin(millis() * 0.0001) * 255)
+    stroke(Math.abs(this.velocity.x) * 255, Math.abs(this.velocity.y) * 255, Math.sin(currTime) * 255);
 
     // Check if particle goes outside the window borders and reset position
     if(this.position.x > windowWidth || this.position.x < 0 || this.position.y > windowHeight || this.position.y < 0) this.position = createVector(Math.random() * windowWidth, Math.random() * windowHeight);
@@ -38,43 +40,16 @@ class particle{
   }
 }
 
-// FPS counter
-function fpsCounter(x, y){
-  let fps = frameRate();
-  let fpsG = 1.0 - Math.exp(-fps * 0.125);
-  let fpsB = 1.0 - Math.exp(-fps * 0.015625);
-
-  fill(255, fpsG * 255, fpsB * 255);
-  textAlign(CENTER);
-  textSize(32);
-  text(fps, x, y);
-}
-
-/*
-function squared(x){
-  return x * x;
-}
-
-function mix(x, y, z){
-  return x + (y - x) *z;
-}
-
-function coordFract(x, axis){
-  // return x;
-  return fract(x / axis) * axis;
-}
-*/
-
-function perlinCustom(x, y){
-  return noise(x * noiseScale, y * noiseScale, 0);
-}
-
 function setup(){
   // Utilize full window size
   createCanvas(windowWidth, windowHeight);
-
   // Set initial background black
   background(0, 0, 0, 255);
+
+  // Set stroke thickness
+  strokeWeight(particleSize);
+  // Don't use fill
+  noFill();
 
   // Generate new particles first on setup
   for(let i = 0; i < particles; i++){
@@ -88,16 +63,12 @@ function windowResized(){
 }
 
 function draw(){
-  noFill();
-  strokeWeight(particleSize);
-
+  // Set alpha to 8 to create trails
   background(0, 0, 0, 8);
   
+  // Load, update, and display particles
   for(let i = 0; i < particleList.length; i++){
     particleList[i].updateParticle();
     particleList[i].displayParticle();
   }
-
-  // noStroke();
-  // fpsCounter(windowWidth * 0.125, windowHeight * 0.125)
 }
